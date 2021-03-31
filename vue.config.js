4852d27d2fd99d31cea04a328989c8721976ae27
867197659@qq.com <867197659@qq.com>
2021-03-31 18:12:41 +0800

    【change】

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ const CompressionPlugin = require("compression-webpack-plugin")
 module.exports = {
     publicPath: './', // 基本路径
     outputDir: 'dist', // 输出文件目录
-    indexPath: '../index.html',
+    // indexPath: '../index.html',
     filenameHashing: true, // 生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存
     lintOnSave: false, // eslint-loader 是否在保存的时候检查
     productionSourceMap: true, // 生产环境是否生成 sourceMap 文件
